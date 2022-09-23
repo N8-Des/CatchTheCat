bc6527cb9381af317913b165d5f8bd4f1f5a0ace
Desjardins <nathan.desjardins@mymail.champlain.edu>
2022-09-23 13:58:43 -0400

    adding start of cat algorithm

--- examples/catchthecat/Cat.cpp
+++ examples/catchthecat/Cat.cpp
@@ -1,10 +1,44 @@
 #include "Cat.h"
 #include "World.h"
 #include <stdexcept>
+#include <queue>
+#include <unordered_map>
+#include <vector>
 
 Point2D Cat::Move(World* world) {
   auto rand = Random::Range(0,5);
   auto pos = world->getCat();
+
+  struct HexTile
+  {
+    Point2D startPosition;
+    Point2D nextPosition;
+    int length;
+  };
+
+  //so basically use a vector and use the HexTile as the queue instead of Point2D. Populate the vector. 
+  std::vector<std::vector<bool>> cameFrom;
+  std::queue<Point2D> frontier;
+  frontier.push(pos);
+  while (!frontier.empty()) 
+  {
+    Point2D curr = frontier.front();
+    frontier.pop();
+    
+    //populate queue in a very silly way
+    frontier.push(world->E(curr));
+    frontier.push(world->NE(curr));
+    frontier.push(world->NW(curr));
+    frontier.push(world->SE(curr));
+    frontier.push(world->SW(curr));
+    frontier.push(world->W(curr));
+
+    //next, go through them all 
+
+
+  }
+  int amt = world->getWorldSideSize();
+
   switch(rand){
     case 0:
       return World::NE(pos);
--- examples/catchthecat/Catcher.cpp
+++ examples/catchthecat/Catcher.cpp
@@ -1,11 +1,14 @@
 #include "Catcher.h"
 #include "World.h"
+#include <queue>
 
 Point2D Catcher::Move(World* world) {
   auto side = world->getWorldSideSize()/2;
   for(;;) {
     Point2D p = {Random::Range(-side, side), Random::Range(-side, side)};
     auto cat = world->getCat();
+
+
     if(cat.x!=p.x && cat.y!=p.y && !world->getContent(p))
       return p;
   }
