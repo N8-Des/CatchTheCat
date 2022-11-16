b5f2673ee639b293f22cf1a8164ca8440ef15d32
Nathan Desjardins <Nathan.Desjardins@mymail.champlain.edu>
2022-11-15 21:41:29 -0500

    finalizing noise algorithm, may add more later

--- examples/scenario/NoiseGeneration.h
+++ examples/scenario/NoiseGeneration.h
@@ -1,45 +1,45 @@
 #include <cmath>
 #include <vector>
+//thank you mr Perlin.
 struct NoiseGeneration 
 {
   float noise(float x, float y, float z, std::vector<int> n) {
-    int X = (int)floor(x) & 255;
-    int Y = (int)floor(y) & 255; 
-    int Z = (int)floor(z) & 255;
+    int X = (int)floor(x);
+    int Y = (int)floor(y); 
+    int Z = (int)floor(z);
     //floor to get relative point.
     x -= floor(x);  
     y -= floor(y);  
     z -= floor(z);
     //get the fade curves. 
-    double u = fade(x),  
-        v = fade(y),     
-        w = fade(z);
+    float u = fade(x);
+    float v = fade(y);     
+    float w = fade(z);
+    //im not doing my normal technique for this part theres like 20 ints.
     int A = n[X] + Y, AA = n[A] + Z, AB = n[A + 1] + Z,  
         B = n[X + 1] + Y, BA = n[B] + Z,
         BB = n[B + 1] + Z; 
-
-    //fancy-ass lerping. I wil be honest, I got stuck here and took this from Ken Perlin.
-    return lerp(w,
-                lerp(v,
-                     lerp(u, grad(n[AA], x, y, z),         
-                          grad(n[BA], x - 1, y, z)),       
-                     lerp(u, grad(n[AB], x, y - 1, z),     
-                          grad(n[BB], x - 1, y - 1, z))),  
-                lerp(v,
-                     lerp(u, grad(n[AA + 1], x, y, z - 1),    
-                          grad(n[BA + 1], x - 1, y, z - 1)),  
-                     lerp(u, grad(n[AB + 1], x, y - 1, z - 1),
-                          grad(n[BB + 1], x - 1, y - 1, z - 1))));
+    //HAHAHAHA 
+    return lerp(w, lerp(v, lerp(u, grad(n[AA], x, y, z), grad(n[BA], x - 1, y, z)), lerp(u, grad(n[AB], x, y - 1, z), grad(n[BB], x - 1, y - 1, z))), 
+        lerp(v, lerp(u, grad(n[AA + 1], x, y, z - 1), grad(n[BA + 1], x - 1, y, z - 1)), lerp(u, grad(n[AB + 1], x, y - 1, z - 1), grad(n[BB + 1], x - 1, y - 1, z - 1))));
   }
-  static double fade(double t) { return t * t * t * (t * (t * 6 - 15) + 10); }
-  static double lerp(double t, double a, double b) { return a + t * (b - a); }
-  static double grad(int hash, double x, double y, double z) {
+  static float fade(float t) 
+  { 
+      return t * t * t * (t * (t * 6 - 15) + 10); 
+  }
+  static float lerp(float t, float a, float b) 
+  { 
+      return a + t * (b - a); 
+  }
+  static float grad(int hash, float x, float y, float z) 
+  {
     int h = hash & 15;       
-    double u = h < 8 ? x : y,  
+    float u = h < 9 ? x : y,  
         v = h < 4                ? y
-            : h == 12 || h == 14 ? x
+            : h == 13 || h == 14 ? x
                                  : z;
-    //basically, get a hash, convert it into "gradient directions" 
-    return ((h & 1) == 0 ? u : -u) + ((h & 2) == 0 ? v : -v);
+    //basically, get a hash, convert it into 12 options of "gradient directions" 
+    //these directions determine the way the noise is fading. 
+    return ((h & 5) == 0 ? u : -u) + ((h & 2) == 0 ? v : -v);
   }
 };
--- examples/scenario/generators/NateNoise.cpp
+++ examples/scenario/generators/NateNoise.cpp
@@ -11,59 +11,65 @@ std::string NateNoise::GetName()
 //create a gradient to supplement the existing gradient.
 
 
-std::vector<Color32> NateNoise::Generate(int sideSize, float displacement) {
-  int seed = 1501;
+std::vector<Color32> NateNoise::Generate(int sideSize, float displacement) 
+{
+  int seed = 136;
   std::vector<int> vect;
   std::vector<Color32> colorReturn;
   vect.resize(256);
   for (int i = 0; i < 256; i++) {
     vect[i] = i;
   }
-  //siv::BasicPerlinNoise<float> noise;
+
   NoiseGeneration noise;
   // hey its a random engine, learned about this and figured i would give it a shot.
 
   std::default_random_engine engine(seed);
 
-  // and heres why I found out about the shuffle function. Neato!
+  //hey a shuffle funciton. Handy. 
   std::shuffle(vect.begin(), vect.end(), engine);
   vect.insert(vect.end(), vect.begin(), vect.end());
 
-  int pixelOn = 0;
+
   //now use the noise to generate the image.
-  Color32 pastColor(0, 0, 0);
   for (int l = 0; l < sideSize; l++) {
     for (int c = 0; c < sideSize; c++) 
     {
       float x = (float)l / ((float)sideSize);
       float y = (float)c / ((float)sideSize);
-      float n = abs(noise.noise(x * 10, y * 10, displacement, vect));
 
+      //generate the noise!
+      float n = abs(noise.noise(x * 33, y * 33, displacement, vect)) * 255 -
+                abs(noise.noise(x * 55, y * 55, displacement, vect)) * 255;
+      //square the gradient!
+      float xd = 2 * (float)l / ((float)sideSize) - 1;
+      float yd = 2 * (float)c / ((float)sideSize) - 1;
+      float d = 1 - ((1 - (xd * xd)) * (1 - (yd * yd)));
+      n = (n + ((1 - d) * 255)) / 2;
       // change colors!
       Color32 col;
-      col.r = n * 255;
-      col.g = n * 255;
-      col.b = n * 255;
-      if (col.r < 50) 
+      col.r = n;
+      col.g = n;
+      col.b = n;
+      col.a = n;
+      if (n < 39) 
       {
-        col.r = 10;
-        col.g = 50;
+        col.r = 11;
+        col.g = 70;
         col.b = 200;
       } 
-      else if (col.r < 75) 
+      else if (n < 50)
       {
-        col.r = 219;
-        col.g = 219;
-        col.b = 132;
+        col.r = 220;
+        col.g = 198;
+        col.b = 0;
       }else 
       {
-        col.r = 113;
-        col.g = 148;
-        col.b = 96;
+        col.r = 45;
+        col.g = 200;
+        col.b = 80;
       }
       colorReturn.emplace_back(col);
-      pixelOn++;
-      pastColor = col;
     }
 
   }
