dc42f12196056afbffff7832cb2bfbab200f7f23
Nathan Desjardins <Nathan.Desjardins@mymail.champlain.edu>
2022-11-16 16:13:46 -0500

    adding full lerping to noise color

--- examples/scenario/generators/NateNoise.cpp
+++ examples/scenario/generators/NateNoise.cpp
@@ -17,29 +17,39 @@ struct GradientSegment
 Color32 rgb(float ratio) 
 {
     ratio /= 100;
-    // kinda big by default so Im gonna turn it into regions.
-    GradientSegment colors[5];
+    if (ratio > 1) 
+    {
+        ratio = 1;
+    }
+    //create the regions.
+    GradientSegment colors[6];
     colors[0].color = Color32(7, 60, 136);
     colors[0].percent = 0;
 
     colors[1].color = Color32(11, 70, 200);
     colors[1].percent = 0.2;
 
-    colors[2].color = Color32(220, 198, 0);
-    colors[2].percent = 0.4;
+    colors[2].color = Color32(200, 212, 148);
+    colors[2].percent = 0.33;
 
     colors[3].color = Color32(45, 200, 80);
-    colors[3].percent = 0.6;
+    colors[3].percent = 0.5;
 
-    colors[4].color = Color32(20, 110, 30);
-    colors[4].percent = 1;
+    colors[4].color = Color32(55, 170, 90);
+    colors[4].percent = 0.75;
+
+    colors[5].color = Color32(84, 82, 82);
+    colors[5].percent = 1;
     
     Color32 finalColor;
-    for (int i = 0; i < 5; i++)
+
+    //lerp.
+
+    for (int i = 0; i < 6; i++)
     {
         if (colors[i].percent <= ratio && colors[i + 1].percent >= ratio)
         {
-            float lerpTime = ratio / colors[i + 1].percent;
+            float lerpTime = (ratio - colors[i].percent) / (colors[i + 1].percent - colors[i].percent);
             finalColor.r = colors[i].color.r + lerpTime * (colors[i + 1].color.r - colors[i].color.r);
             finalColor.g = colors[i].color.g + lerpTime * (colors[i + 1].color.g - colors[i].color.g);
             finalColor.b = colors[i].color.b + lerpTime * (colors[i + 1].color.b - colors[i].color.b);
@@ -74,13 +84,13 @@ std::vector<Color32> NateNoise::Generate(int sideSize, float displacement)
     for (int c = 0; c < sideSize; c++) 
     {
       //generate the noise!
-      float n = (abs(noise.octave3D(c / 50.0, l / 50.0, displacement, 3)) - abs(noise.octave3D(c / 136.0, l / 136.0, displacement, 10))) * 255;
+      float n = (abs(noise.octave3D(c / 50.0, l / 50.0, displacement, 3)) - abs(noise.octave3D(c / 136.0, l / 136.0, displacement, 9))) * 255;
             
       //square the gradient!
       float x = 2 * (float)l / ((float)sideSize) - 1;
       float y = 2 * (float)c / ((float)sideSize) - 1;
       float d = 1 - ((1 - (x * x)) * (1 - (y * y)));
-      n = abs(n + ((1 - d) * 255)) / 4;
+      n = abs(n + ((1 - d) * 255)) / 5;
       //change colors!
       Color32 col;
       col.r = n;
@@ -91,59 +101,6 @@ std::vector<Color32> NateNoise::Generate(int sideSize, float displacement)
       colorReturn.emplace_back(col);
     }
   }
-  //attempted lerping. It didn't work. 
-  /* for (int i = 0; i < colorReturn.size(); i++) 
-  {
-    Color32 lerpCol; 
-    //right
-    if (i - 1 < 0) 
-    {
-      lerpCol = colorReturn[i];
-    }
-    else 
-    {
-      lerpCol = colorReturn[i - 1];
-    }
-    colorReturn[i].r = abs(colorReturn[i].r - lerpCol.r) * 0.5 + colorReturn[i].r;
-    colorReturn[i].g = abs(colorReturn[i].g - lerpCol.g) * 0.5 + colorReturn[i].g;
-    colorReturn[i].b = abs(colorReturn[i].b - lerpCol.b) * 0.5 + colorReturn[i].b;
-    //top
-    if (i - sideSize < 0) 
-    {
-      lerpCol = colorReturn[i];
-    } 
-    else 
-    {
-      lerpCol = colorReturn[i - sideSize];
-    }
-    colorReturn[i].r = abs(colorReturn[i].r - lerpCol.r) * 0.5 + colorReturn[i].r;
-    colorReturn[i].g = abs(colorReturn[i].g - lerpCol.g) * 0.5 + colorReturn[i].g;
-    colorReturn[i].b = abs(colorReturn[i].b - lerpCol.b) * 0.5 + colorReturn[i].b;
-    //left
-    if (i + 1 >= vect.size()) 
-    {
-      lerpCol = colorReturn[i];
-    }
-    else 
-    {
-      lerpCol = colorReturn[i + 1];
-    }
-    colorReturn[i].r = abs(colorReturn[i].r - lerpCol.r) * 0.5 + colorReturn[i].r;
-    colorReturn[i].g = abs(colorReturn[i].g - lerpCol.g) * 0.5 + colorReturn[i].g;
-    colorReturn[i].b = abs(colorReturn[i].b - lerpCol.b) * 0.5 + colorReturn[i].b;
-    //bottom
-    if (i + sideSize >= vect.size()) 
-    {
-      lerpCol = colorReturn[i];
-    } 
-    else 
-    {
-      lerpCol = colorReturn[i + sideSize];
-    }
-    colorReturn[i].r = abs(colorReturn[i].r - lerpCol.r) * 0.5 + colorReturn[i].r;
-    colorReturn[i].g = abs(colorReturn[i].g - lerpCol.g) * 0.5 + colorReturn[i].g;
-    colorReturn[i].b = abs(colorReturn[i].b - lerpCol.b) * 0.5 + colorReturn[i].b;
-  }*/
   return colorReturn;
 }
 
